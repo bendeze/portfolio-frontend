c570389b7bf611d432382a29671a1386eb8c35e2
Bonheur Ndeze <bonheurndezenc@gmail.com>
2026-01-02 13:03:51 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from "@/components/sections/hero";
 import { AboutSection } from "@/components/sections/about";
-import { ProjectList } from "@/features/projects/components/project-hero-list";
+// import { ProjectList } from "@/features/projects/components/project-hero-list";
 import { SkillsSection } from "@/components/sections/skills";
 import { ContactSection } from "@/components/sections/contact"
 
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <Hero />
       <AboutSection />
-      <ProjectList />
+      {/* <ProjectList /> */}
       <SkillsSection />
       <ContactSection />
     </div>

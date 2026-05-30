340bcfc3c53c26e46ff5f9c46b60735db1c6d3f2
bonheurNE07 <bonheurndezenc@gmail.com>
2026-05-31 01:13:46 +0200

    Establish generic page URLs list navigation configuration schema

--- components/shared/navbar/nav-config.ts
+++ components/shared/navbar/nav-config.ts
@@ -1,6 +1,5 @@
 export const navigation = [
   { name: 'About', href: '#about' },
-  { name: 'Skills', href: '#skills' },
   { name: 'Projects', href: '/projects' },
   { name: 'Blog', href: '/blog' },
   { name: 'Contact', href: '/contact' },

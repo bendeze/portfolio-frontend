e49eabd84f143b74f6c5325d86a2ba8e6171489a
Ndeze Bonheur Emmanuel <bonheurndezenc@gmail.com>
2026-06-21 21:55:34 +0200

    Add Mastodon link to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100..900;1,100..900&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet" />
+        <a rel="me" href="https://mastodon.social/@bendeze">Mastodon</a>
       </head>
       <body
         suppressHydrationWarning
